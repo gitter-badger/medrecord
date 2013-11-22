5402b20a9fbffed4be15e0fadf9242bc68c727cc
Leo Simons <mail@leosimons.com>
2013-11-22 17:38:06 +0100

    Need config files

--- java/medrecord-server/deploy.sh
+++ java/medrecord-server/deploy.sh
@@ -3,7 +3,12 @@
 # this is going to be replaced with a gradle script in the near future, 
 # then you can do something like 'gradle deployDev'
 
-set -e
-(cd medrecord-server-deb && gradle buildDebianPackage)
+SERVER=medrecord.dev.medvision360.org
+CONF_DIR=/etc/medvision360/medrecord
 
-#ssh root@profileserver2.dev.medvision360.org /usr/local/bin/ddeploy < deb/build/mv-profileserver2-2.0.0.deb 
+set -e
+echo \(cd medrecord-server-deb && gradle buildDebianPackage\)
+
+echo ssh root@${SERVER} /usr/local/bin/ddeploy \< deb/build/mv-medrecord-2.0.0.deb 
+echo scp configuration-server.properties root@${SERVER}:${CONF_DIR}/configuration.properties
+echo scp logback-server.xml root@$SERVER:${CONF_DIR}/logback.xml
